65cb74757468da99a5c4a529a4cab9fc1f939d3f
todoapuestas <todoapuestas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-18 20:41:07 +0000

    Initial commit. Based on version 1.1.0
    git-svn-id: https://plugins.svn.wordpress.org/tipster-tap/trunk@992857 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,77 @@
+=== Tipster TAP ===
+Contributors: mrbrazzi, todoapuestas
+Donate link: http://todoapuestas.org/
+Tags: tipster, picks
+Requires at least: 3.5.1
+Tested up to: 3.9.2
+Stable tag: 1.1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Manage picks, tipsters.
+
+== Description ==
+
+This plugin is to manage pick, tipsters.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+= Using The WordPress Dashboard =
+
+1. Navigate to the 'Add New' in the plugins dashboard
+2. Search for 'plugin-name'
+3. Click 'Install Now'
+4. Activate the plugin on the Plugin dashboard
+
+= Uploading in WordPress Dashboard =
+
+1. Navigate to the 'Add New' in the plugins dashboard
+2. Navigate to the 'Upload' area
+3. Select `plugin-name.zip` from your computer
+4. Click 'Install Now'
+5. Activate the plugin in the Plugin dashboard
+
+= Using FTP =
+
+1. Download `plugin-name.zip`
+2. Extract the `plugin-name` directory to your computer
+3. Upload the `plugin-name` directory to the `/wp-content/plugins/` directory
+4. Activate the plugin in the Plugin dashboard
+
+
+== Frequently Asked Questions ==
+
+Nothing for now
+
+
+== Screenshots ==
+
+Nothing for now
+
+
+== Changelog ==
+
+= 1.0 =
+* Initial release.
+
+= 1.1.0 =
+* Updated some Pick's metabox fields.
+* Created a plugin option page to upgrade Pick information to the new postmeta definitions.
+
+
+== Upgrade Notice ==
+
+Upgrade to version 1.1.0 as soon as posible. See Changelog section for details
+
+
+== Arbitrary section ==
+
+Nothing for now
+
+
+== Updates ==
+
+The basic structure of this plugin was cloned from the [WordPress-Plugin-Boilerplate](https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate) project.
+This plugin supports the [GitHub Updater](https://github.com/afragen/github-updater) plugin, so if you install that, this plugin becomes automatically updateable direct from GitHub. Any submission to WP.org repo will make this redundant.
---
+++ public/class-tipster-tap.php
@@ -0,0 +1,356 @@
+<?php
+/**
+ * Plugin Name.
+ *
+ * @package   Tipster_TAP
+ * @author    Alain Sanchez <asanchezg@inetzwerk.com>
+ * @license   GPL-2.0+
+ * @link      http://www.inetzwerk.com
+ * @copyright 2014 Alain Sanchez
+ */
+
+/**
+ * Plugin class. This class should ideally be used to work with the
+ * public-facing side of the WordPress site.
+ *
+ * If you're interested in introducing administrative or dashboard
+ * functionality, then refer to `class-tipster-tap-admin.php`
+ *
+ * @TODO: Rename this class to a proper name for your plugin.
+ *
+ * @package Tipster_TAP
+ * @author  Your Name <email@example.com>
+ */
+class Tipster_TAP {
+
+	/**
+	 * Plugin version, used for cache-busting of style and script file references.
+	 *
+	 * @since   1.0.0
+	 *
+	 * @var     string
+	 */
+	const VERSION = '1.0.0';
+
+	/**
+	 * @TODO - Rename "tipster-tap" to the name of your plugin
+	 *
+	 * Unique identifier for your plugin.
+	 *
+	 *
+	 * The variable name is used as the text domain when internationalizing strings
+	 * of text. Its value should match the Text Domain file header in the main
+	 * plugin file.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @var      string
+	 */
+	protected $plugin_slug = 'tipster-tap';
+
+	/**
+	 * Instance of this class.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @var      object
+	 */
+	protected static $instance = null;
+
+    private $default_options;
+
+	/**
+	 * Initialize the plugin by setting localization and loading public scripts
+	 * and styles.
+	 *
+	 * @since     1.0.0
+	 */
+	private function __construct() {
+
+		// Load plugin text domain
+		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
+
+		// Activate plugin when new blog is added
+		add_action( 'wpmu_new_blog', array( $this, 'activate_new_site' ) );
+
+		// Load public-facing style sheet and JavaScript.
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+
+        /**
+         * Define the default options
+         *
+         * @since     1.0
+         */
+        $this->default_options = array(
+            'url_sync_link_bookies' => 'http://www.todoapuestas.org/wp-services/getAllBookiesLicenciaEspJson.php',
+            'url_sync_link_deportes' => 'http://www.todoapuestas.org/wp-services/getDeportesJson.php',
+            'url_sync_link_competiciones' => 'http://www.todoapuestas.org/wp-services/getCompeticionesJson.php',
+        );
+
+        /* Define custom functionality.
+         * Refer To http://codex.wordpress.org/Plugin_API#Hooks.2C_Actions_and_Filters
+         *
+         * add_action ( 'hook_name', 'your_function_name', [priority], [accepted_args] );
+         *
+         * add_filter ( 'hook_name', 'your_filter', [priority], [accepted_args] );
+         */
+
+        add_action( 'sync_hourly_event', array( $this, 'remote_sync' ) );
+        add_action( 'wp' , array( $this, 'active_remote_sync'));
+	}
+
+	/**
+	 * Return the plugin slug.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @return    Plugin slug variable.
+	 */
+	public function get_plugin_slug() {
+		return $this->plugin_slug;
+	}
+
+	/**
+	 * Return an instance of this class.
+	 *
+	 * @since     1.0.0
+	 *
+	 * @return    object    A single instance of this class.
+	 */
+	public static function get_instance() {
+
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Fired when the plugin is activated.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @param    boolean    $network_wide    True if WPMU superadmin uses
+	 *                                       "Network Activate" action, false if
+	 *                                       WPMU is disabled or plugin is
+	 *                                       activated on an individual blog.
+	 */
+	public static function activate( $network_wide ) {
+
+		if ( function_exists( 'is_multisite' ) && is_multisite() ) {
+
+			if ( $network_wide  ) {
+
+				// Get all blog ids
+				$blog_ids = self::get_blog_ids();
+
+				foreach ( $blog_ids as $blog_id ) {
+
+					switch_to_blog( $blog_id );
+					self::single_activate();
+
+					restore_current_blog();
+				}
+
+			} else {
+				self::single_activate();
+			}
+
+		} else {
+			self::single_activate();
+		}
+
+	}
+
+	/**
+	 * Fired when the plugin is deactivated.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @param    boolean    $network_wide    True if WPMU superadmin uses
+	 *                                       "Network Deactivate" action, false if
+	 *                                       WPMU is disabled or plugin is
+	 *                                       deactivated on an individual blog.
+	 */
+	public static function deactivate( $network_wide ) {
+
+		if ( function_exists( 'is_multisite' ) && is_multisite() ) {
+
+			if ( $network_wide ) {
+
+				// Get all blog ids
+				$blog_ids = self::get_blog_ids();
+
+				foreach ( $blog_ids as $blog_id ) {
+
+					switch_to_blog( $blog_id );
+					self::single_deactivate();
+
+					restore_current_blog();
+
+				}
+
+			} else {
+				self::single_deactivate();
+			}
+
+		} else {
+			self::single_deactivate();
+		}
+
+	}
+
+	/**
+	 * Fired when a new site is activated with a WPMU environment.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @param    int    $blog_id    ID of the new blog.
+	 */
+	public function activate_new_site( $blog_id ) {
+
+		if ( 1 !== did_action( 'wpmu_new_blog' ) ) {
+			return;
+		}
+
+		switch_to_blog( $blog_id );
+		self::single_activate();
+		restore_current_blog();
+
+	}
+
+	/**
+	 * Get all blog ids of blogs in the current network that are:
+	 * - not archived
+	 * - not spam
+	 * - not deleted
+	 *
+	 * @since    1.0.0
+	 *
+	 * @return   array|false    The blog ids, false if no matches.
+	 */
+	private static function get_blog_ids() {
+
+		global $wpdb;
+
+		// get an array of blog ids
+		$sql = "SELECT blog_id FROM $wpdb->blogs
+			WHERE archived = '0' AND spam = '0'
+			AND deleted = '0'";
+
+		return $wpdb->get_col( $sql );
+
+	}
+
+	/**
+	 * Fired for each blog when the plugin is activated.
+	 *
+	 * @since    1.0.0
+	 */
+	private static function single_activate() {
+        add_option('tipster_tap_remote_info', self::get_instance()->default_options);
+        add_option('tipster_tap_bookies', array());
+        add_option('tipster_tap_deportes', array());
+        add_option('tipster_tap_competiciones', array());
+
+        // execute initial synchronization
+        self::get_instance()->remote_sync();
+	}
+
+	/**
+	 * Fired for each blog when the plugin is deactivated.
+	 *
+	 * @since    1.0.0
+	 */
+	private static function single_deactivate() {
+        remove_action( 'sync_hourly_event', array( self::$instance, 'remote_sync' ) );
+        remove_action( 'wp' , array( self::$instance, 'active_remote_sync'));
+
+        delete_option('tipster_tap_remote_info');
+        delete_option('tipster_tap_bookies');
+        delete_option('tipster_tap_deportes');
+        delete_option('tipster_tap_competiciones');
+	}
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		$domain = $this->plugin_slug;
+		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
+
+		load_textdomain( $domain, trailingslashit( WP_LANG_DIR ) . $domain . '/' . $domain . '-' . $locale . '.mo' );
+		load_plugin_textdomain( $domain, FALSE, basename( plugin_dir_path( dirname( __FILE__ ) ) ) . '/languages/' );
+
+	}
+
+	/**
+	 * Register and enqueue public-facing style sheet.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles() {
+		wp_enqueue_style( $this->plugin_slug . '-plugin-styles', plugins_url( 'assets/css/public.css', __FILE__ ), array(), self::VERSION );
+	}
+
+	/**
+	 * Register and enqueues public-facing JavaScript files.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts() {
+		wp_enqueue_script( $this->plugin_slug . '-plugin-script', plugins_url( 'assets/js/public.js', __FILE__ ), array( 'jquery' ), self::VERSION );
+	}
+
+    /**
+     * Activate remote synchronization hourly
+     *
+     * @since   1.0
+     */
+    public function active_remote_sync() {
+        if ( !wp_next_scheduled( 'sync_hourly_event' ) ) {
+            wp_schedule_event(time(), 'hourly', 'sync_hourly_event');
+        }
+    }
+
+    /**
+     * Execute synchronizations from todoapuestas.org server
+     *
+     * @since   1.0
+     * @return array|void
+     */
+    public function remote_sync() {
+        $option = get_option('tipster_tap_remote_info', $this->default_options);
+
+        $url_sync_link_bookies = esc_url($option['url_sync_link_bookies']);
+        $bookies = trim(@file_get_contents($url_sync_link_bookies));
+        $list_bookies = json_decode($bookies, true);
+
+        if(!empty($list_bookies)){
+            update_option('tipster_tap_bookies', $list_bookies);
+        }
+
+        $url_sync_link_deportes = esc_url($option['url_sync_link_deportes']);
+        $deportes = trim(@file_get_contents($url_sync_link_deportes));
+        $list_deportes = json_decode($deportes, true);
+
+        if(!empty($list_deportes)){
+            update_option('tipster_tap_deportes', $list_deportes);
+        }
+
+        $url_sync_link_competiciones = esc_url($option['url_sync_link_competiciones']);
+        $competiciones = trim(@file_get_contents($url_sync_link_competiciones));
+        $list_competiciones = json_decode($competiciones, true);
+
+        if(!empty($list_deportes)){
+            update_option('tipster_tap_competiciones', $list_competiciones);
+        }
+    }
+
+}
